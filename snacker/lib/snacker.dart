3029eda185cdc33e514a83c074845c0c0be53270
Nowrose Muhammad (Ragib) <ragibn5@gmail.com>
2026-08-30 12:21:29 +0600

    DFP-63 Refactor: Delegate snack UI construction to `SnackBarBuilder`
    - Introduce `SnackBarBuilder` and `DefaultSnackBarBuilder` to decouple snack appearance from presentation logic.
    - Move UI construction methods (`buildSnackBar`, `buildSnackContent`, and `getSnackBarBackgroundColor`) from `Snacker` to `SnackBarBuilder`.
    - Update `Snacker` to require a `SnackBarBuilder` and use it in `showTextSnack` to generate the `SnackBar` widget.
    - Update `ScaffoldMessengerSnacker` constructor to accept an optional `snackBarBuilder`.
    - Refactor unit tests to use custom `SnackBarBuilder` implementations instead of subclassing `Snacker` for UI customization.
    - Export `SnackBarBuilder` and `DefaultSnackBarBuilder` in `snacker.dart`.
    - Add documentation comments to `ScaffoldMessengerSnacker` and `Snacker`.

--- snacker/lib/snacker.dart
+++ snacker/lib/snacker.dart
@@ -4,4 +4,5 @@ library;
 export 'src/enums/snack_type.dart';
 export 'src/models/snack_data.dart';
 export 'src/scaffold_messenger_snacker.dart';
+export 'src/snack_bar_builder.dart';
 export 'src/snacker.dart';
--- snacker/lib/src/scaffold_messenger_snacker.dart
+++ snacker/lib/src/scaffold_messenger_snacker.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:snacker/src/snack_bar_builder.dart';
 import 'package:snacker/src/snacker.dart';
 
+/// A [Snacker] that shows snacks via a [ScaffoldMessenger] referenced by a
+/// [GlobalKey].
+///
+/// Pass [snackBarBuilder] to customize the snack appearance.
 class ScaffoldMessengerSnacker extends Snacker {
   final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey;
 
-  const ScaffoldMessengerSnacker(this._scaffoldMessengerKey);
+  const ScaffoldMessengerSnacker(
+    this._scaffoldMessengerKey, {
+    super.snackBarBuilder = const DefaultSnackBarBuilder(),
+  });
 
   @override
   BuildContext getCurrentContext() {
